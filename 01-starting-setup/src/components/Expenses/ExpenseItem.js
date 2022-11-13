cb8b70facbeb8ae53563ad60cddc77d2b84bf6ab
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-13 16:45:15 +0530

    button click event handler

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const handleClick = () => {
+    console.log("Hi!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 }
