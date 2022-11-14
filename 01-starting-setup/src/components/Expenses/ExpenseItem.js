0d7b83c286064e6624b2691f20fd70d8e5270fbf
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-14 07:45:57 +0530

    update title on click

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,21 @@ import "./ExpenseItem.css";
 import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  // let title = props.title;
+  const [title, setTitle] = useState(props.title);
   const handleClick = () => {
-    console.log("Hi!");
+    // title = "Updated";
+    // console.log("Hi!");
+    setTitle("Updated!");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={handleClick}>Change Title</button>
