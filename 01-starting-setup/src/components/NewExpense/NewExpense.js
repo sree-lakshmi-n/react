b8c121df1b75ffefb527455a62016a1d91ce4db2
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-14 08:11:59 +0530

    ExpenseForm component

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,23 +1,10 @@
 import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 const NewExpense = () => {
   return (
-    <form>
-      <div className="new-expense__controls">
-        <label>Title</label>
-        <input type="text"></input>
-      </div>
-      <div className="new-expense__controls">
-        <label>Amount</label>
-        <input type="number" min="0.01" step="0.01"></input>
-      </div>
-      <div className="new-expense__controls">
-        <label>Date</label>
-        <input type="date" min="2019-01-01" max="2022-12-31"></input>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    <div>
+      <ExpenseForm />
+    </div>
   );
 };
 export default NewExpense;
