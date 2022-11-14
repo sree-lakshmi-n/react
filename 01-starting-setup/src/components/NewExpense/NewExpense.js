0ce22ac8ff764973caaeb8fae9b1a9f8012dd97f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-14 09:18:10 +0530

    added classname new-expense

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -2,7 +2,7 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 const NewExpense = () => {
   return (
-    <div>
+    <div className="new-expense">
       <ExpenseForm />
     </div>
   );
