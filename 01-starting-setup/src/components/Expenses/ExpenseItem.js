deda42f43f8776c756b48f3b9f976ac9473dc322
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-04 18:36:14 +0530

    enclosed in li tag since ExpenseList is a ul container

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -12,14 +12,16 @@ function ExpenseItem(props) {
   //   setTitle("Updated!");
   // };
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      {/* <button onClick={handleClick}>Change Title</button> */}
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        {/* <button onClick={handleClick}>Change Title</button> */}
+      </Card>
+    </li>
   );
 }
 
