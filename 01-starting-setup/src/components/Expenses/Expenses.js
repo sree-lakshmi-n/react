12fbc0a4eadc5414d1ebd72dcaf42de451f02172
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-04 17:45:21 +0530

    Conditional content added (3 ways)

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -14,6 +14,22 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  /* Conditional content
+      3. Using if condition in component function, outside return statement
+      - Makes JSX code lean
+      - More readable
+  */
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      ></ExpenseItem>
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -21,6 +37,13 @@ const Expenses = (props) => {
           select={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        {/* Conditional content 
+        3. Using if condition in component function,
+        outside return statement 
+        - Makes JSX code lean 
+        - More readable 
+        */}
+        {expensesContent}
         {/* Conditional content
         1. Using ternary operators
         */}
