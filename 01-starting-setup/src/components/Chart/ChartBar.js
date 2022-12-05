82b4f9aac152ae8ceaf93e27a24b208d24262abd
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-05 23:03:21 +0530

    max renamed to maxValue and props.label changed to props.month

--- 01-starting-setup/src/components/Chart/ChartBar.js
+++ 01-starting-setup/src/components/Chart/ChartBar.js
@@ -2,8 +2,8 @@ import "./ChartBar.css";
 
 const ChartBar = (props) => {
   let barFillHeight = "0%";
-  if (props.max > 0) {
-    barFillHeight = Math.round((props.value / props.max) * 100) + "%";
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
     <div className="chart-bar">
@@ -13,7 +13,7 @@ const ChartBar = (props) => {
           style={{ height: barFillHeight }}
         ></div>
       </div>
-      <div className="chart-bar__label">{props.label}</div>
+      <div className="chart-bar__label">{props.month}</div>
     </div>
   );
 };
